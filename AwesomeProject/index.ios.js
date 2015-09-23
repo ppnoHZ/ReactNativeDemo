13804ee1eb8aa5d236111c47ed4eb997083cac20
zhoudd-mac <zhoudd@stark.tm>
2015-09-23 09:44:08 +0800

    更新

--- AwesomeProject/index.ios.js
+++ AwesomeProject/index.ios.js
@@ -6,13 +6,13 @@
 
 var React = require('react-native');
 var {
-  AppRegistry,
-   Image,
-   ListView,
-  StyleSheet,
-  Text,
-  View,
-} = React;
+    AppRegistry,
+    Image,
+    ListView,
+    StyleSheet,
+    Text,
+    View,
+    } = React;
 
 // var MOCKED_MOVIES_DATA = [
 //   {title: 'Title', year: '2015', posters: {thumbnail: 'http://i.imgur.com/UePbdph.jpg'}},
@@ -27,95 +27,90 @@ var REQUEST_URL = API_URL + PARAMS;
 // var REQUEST_URL = 'https://raw.githubusercontent.com/facebook/react-native/master/docs/MoviesExample.json';
 
 var AwesomeProject = React.createClass({
-  getInitialState:function(){
-    return {
-      dataSource:new ListView.DataSource({
-        rowHasChanger:(row1,row2)=>row1!==row2,
-      }),
-      loaded:false,
-    };
-  },
-  componentDidMount: function() {
+    getInitialState: function () {
+        return {
+            dataSource: new ListView.DataSource({
+                rowHasChanged: (row1, row2)=>row1 !== row2,
+            }),
+            loaded: false,
+        };
+    },
+    componentDidMount: function () {
         this.fetchData();
     },
-  fetchData: function() {
+    fetchData: function () {
         fetch(REQUEST_URL)
             .then((response) => response.json())
             .then((responseData) => {
                 this.setState({
-                    movies: this.state.dataSource.cloneWithRows(responseData.movies),
-                    loaded:true,
+                    dataSource: this.state.dataSource.cloneWithRows(responseData.movies),
+                    loaded: true,
                 });
             })
             .done();
     },
-  render: function() {
+    render: function () {
 
-      if(!this.state.movies)
-      {
-        return this.renderLoadingView();
-      }
-      return (
-
-            <ListView dataSource={this.state.dataSource}
-                      renderRow={this.renderMovie}
-                      style{styles.listView}
-            />
-
+        if (!this.state.dataSource) {
+            return this.renderLoadingView();
+        }
+        return (
+            <ListView dataSource={this.state.dataSource} renderRow={this.renderMovie} style={styles.listView} />
         );
 
 
-  },
-  renderLoadingView:function(){
-    return (
-      <View style={styles.container}>
-        <Text>
-            Loading Movies....
-        </Text>
-      </View>)
-  },
-  renderMovie:function  (movie) {
-    return (
+    },
+    renderLoadingView: function () {
+        return (
             <View style={styles.container}>
-            <Image
-                source={{uri: movie.posters.thumbnail}}
-                style={styles.thumbnail}
-            />
-            <View style={styles.rightContainer}>
-                <Text style={styles.title}>{movie.title}</Text>
-                <Text style={styles.year}>{movie.year}</Text>
+                <Text>
+                    Loading Movies....
+                </Text>
+            </View>)
+    },
+    renderMovie: function (movie) {
+        //console.log(JSON.stringify(movie));
+        return (
+            <View style={styles.container}>
+                <Image
+                    source={{uri: movie.posters.thumbnail}}
+                    style={styles.thumbnail}
+                    />
+                <View style={styles.rightContainer}>
+                    <Text style={styles.title}>{movie.title}</Text>
+                    <Text style={styles.year}>{movie.year}</Text>
+                </View>
             </View>
-        </View>
-    );
-  }
+        );
+    }
 });
 
 var styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    flexDirection: 'row',
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
-  },
- thumbnail:{
-  width:52,
-  height:81
- },
+    container: {
+        flex: 1,
+        flexDirection: 'row',
+        justifyContent: 'center',
+        alignItems: 'center',
+        backgroundColor: '#F5FCFF',
+    },
+    thumbnail: {
+        width: 52,
+        height: 81
+    },
     rightContainer: {
         flex: 1,
     },
-     title: {
+    title: {
         fontSize: 20,
         marginBottom: 8,
         textAlign: 'center',
     },
     year: {
-    textAlign: 'center',
+        textAlign: 'center',
     },
-    listView:{
-      paddingTop:20,
-      backgroundColor:'#F5FCFF'
+    listView: {
+        paddingTop: 20,
+        backgroundColor: '#F5FCFF'
     }
 });
 
